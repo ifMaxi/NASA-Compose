59f3a233d5dd1419bbfd5792862403adf8cea755
Maxi Pelizzoni <maxi96p@gmail.com>
2023-11-30 14:40:46 -0300

    Fixed MissingFieldException

--- app/src/main/java/com/maxidev/nasacompose/data/model/ApodModel.kt
+++ app/src/main/java/com/maxidev/nasacompose/data/model/ApodModel.kt
@@ -7,7 +7,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ApodModel(
     @SerialName("copyright")
-    val copyright: String,
+    val copyright: String? = null,
     @SerialName("date")
     val date: String,
     @SerialName("explanation")
